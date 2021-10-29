99a715e94220ac393df5e967a0c6aed3ef336e10
ebront08 <ebront08@gmail.com>
2021-10-29 14:43:27 -0400

    added resolvers

--- client/src/index.js
+++ client/src/index.js
@@ -32,3 +32,10 @@ const typeDefs = gql`
     books: [Book]
   }
 `;
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
