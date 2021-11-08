d4f80cfd6dc3976b0d18540807dd2e30ebea7c24
ebront08 <ebront08@gmail.com>
2021-11-08 12:46:39 -0500

    changed code

--- server/utils/schema/typeDefs.js
+++ server/utils/schema/typeDefs.js
@@ -1,50 +1,46 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    thoughts: [Thought]!
-  }
-
-  type Thought {
-    _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
-  }
-
-  type Comment {
-    _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
-  }
-
-  type Auth {
-    token: ID!
-    user: User
-  }
-
-  type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
-    me: User
-  }
-
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
-  }
+    type Book {
+        _id: String
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        users: [User]!
+        user(userId: ID!): User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        
+        addBook(
+            userId: ID!
+            authors: [String]!
+            description: String!
+            bookId: String!
+            image: String!
+            link: String!
+            title: String!
+        ): User
+        removeUser(userId: ID!): User
+        removeBook(userId: String!, bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
