7d11334a9b68af33df77b7d025ce2d6623c6f269
ebront08 <ebront08@gmail.com>
2021-11-11 14:38:20 -0500

    heroku

---
+++ client/build/precache-manifest.da2ab03a905b1e9ea3af9d662b99223b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a255e10d14b38de351e14909babd7ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d2ce6291652c6688050",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "3f138d5761f900db807c",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "8d2ce6291652c6688050",
+    "url": "/static/js/2.612fbadc.chunk.js"
+  },
+  {
+    "revision": "f958918813a43cb2c476be281ae22f8f",
+    "url": "/static/js/2.612fbadc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f138d5761f900db807c",
+    "url": "/static/js/main.a6bd3f1d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
