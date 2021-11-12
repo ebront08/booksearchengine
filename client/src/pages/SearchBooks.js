fea571c743ad05e50766248142f12cf26c7d8e81
ebront08 <ebront08@gmail.com>
2021-11-12 10:18:28 -0500

    added

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,7 +10,7 @@ import {
 } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-import { searchGoogleBooks } from "../utils/API.js";
+import { searchGoogleBooks } from "../utils/googleBooksApi";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 import { useMutation } from "@apollo/client";
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -3,68 +3,68 @@ const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find();
-        },
-        user: async (parent, { userId }) => {
-            return User.findOne({ _id: userId });
-        },
+  Query: {
+    users: async () => {
+      return User.find();
     },
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-        },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-            if (!user) {
-                throw new AuthenticationError("No user with this email found!");
-            }
-            const correctPw = await user.isCorrectPassword(password);
-            if (!correctPw) {
-                throw new AuthenticationError("Incorrect password!");
-            }
-            const token = signToken(user);
-            return { token, user };
-        },
-        addBook: async (
-            parent,
-            { userId, authors, description, bookId, image, link, title }
-        ) => {
-            const book = {
-                authors,
-                description,
-                bookId,
-                image,
-                link,
-                title,
-            };
-            console.log(book);
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addBook: async (
+      parent,
+      { userId, authors, description, bookId, image, link, title }
+    ) => {
+      const book = {
+        authors,
+        description,
+        bookId,
+        image,
+        link,
+        title,
+      };
+      console.log(book);
 
-            return User.findOneAndUpdate(
-                { _id: userId },
-                {
-                    $addToSet: { savedBooks: book },
-                },
-                {
-                    new: true,
-                    runValidators: true,
-                }
-            );
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $addToSet: { savedBooks: book },
         },
-        removeUser: async (parent, { userId }) => {
-            return User.findOneAndDelete({ _id: userId });
-        },
-        removeBook: async (parent, { userId, bookId }) => {
-            return User.findOneAndUpdate(
-                { _id: userId },
-                { $pull: { savedBooks: { bookId: bookId } } },
-                { new: true }
-            );
-        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
     },
+    removeUser: async (parent, { userId }) => {
+      return User.findOneAndDelete({ _id: userId });
+    },
+    removeBook: async (parent, { userId, bookId }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+    },
+  },
 };
 
 module.exports = resolvers;
